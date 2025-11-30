06f960e1149cdb7a3db3b9a15237e9d93f2d236d
jalotra <jalotrashivam9@gmail.com>
2025-11-30 15:25:56 +0530

    initial support for page

---
+++ app/lib/uploadFiles.ts
@@ -0,0 +1,13 @@
+
+
+const uploadFiles = async (files: FileList) => {
+  const formData = new FormData();
+  for (const file of files) {
+    formData.append("files", file);
+  }
+  const response = await fetch("/api/upload", {
+    method: "POST",
+    body: formData,
+  });
+  return response.json();
+};
